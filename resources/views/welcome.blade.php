c9c9da178e56c52f861756de15141c360624aa77
012kirby210 <012kirby210@gmail.com>
2022-08-15 18:46:36 +0200

    Tensorflow spam detection part 1 https://developers.google.com/codelabs/tensorflowjs-comment-spam-detection

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <body class="flex flex-col mx-auto items-center">
+    <h1 class="text-4xl my-6">Spam detector</h1>
+    <textarea id="textarea-spam-detection" cols="50" rows="10" class="my-4 rounded border border-orange-400 shadow-sm">
+
+    </textarea>
+    <button id="button-try" class="p-5 border-white border-solid rounded-xl text-center text-white uppercase bg-amber-600 hover:bg-amber-400" id="submit-text" type="button">Try it</button>
+    <script src="/scripts/spam.detector.js"></script>
+    </body>
+</x-layout>
